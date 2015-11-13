22155deb2bb0b0236ac358c143b6abf4663bfe69
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-11-12 21:29:06 -0500

    use the server

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,6 @@ public class Main {
         new User("Carlos", "1234");
         new User("Rahul", "1234");
 
-        SessionManager sm = new SessionManager();
-        String htoken = sm.authenticate("Hector", "1234");
-        String ctoken = sm.authenticate("Carlos", "1234");
-
-        System.out.println(htoken);
-        System.out.println(ctoken);
-
-        System.out.println(sm.checkToken(htoken));
-        System.out.println(sm.checkToken(ctoken));
-        System.out.println(sm.checkToken("wrong_token"));
+        Server.run();
     }
 }
