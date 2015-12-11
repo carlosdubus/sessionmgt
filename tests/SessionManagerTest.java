12c65210ec9dc6330ccd06d620fe5491358f3c16
Carlos Dubus <carlos.garciadm@gmail.com>
2015-12-10 19:51:27 -0500

    code cleanup

--- tests/SessionManagerTest.java
+++ tests/SessionManagerTest.java
@@ -1,23 +1,22 @@
 import org.junit.*;
 import junit.framework.JUnit4TestAdapter;
- 
+
 public class SessionManagerTest{
- 
-  @Before  
+  @Before
   public void setUp(){
     new User("Hue", "1234", "admin");
   }
- 
+
   @Test
-  public void testAuthenticationWithValidCredential(){
+  public void testAuthenticationWithValidCredentials(){
     SessionManager s = new SessionManager();
     Assert.assertNotNull(s.authenticate("Hue", "1234"));
   }
- 
+
   @After
   public void tearDown(){
   }
- 
+
   public static junit.framework.Test suite(){
     return new JUnit4TestAdapter(SessionManagerTest.class);
   }
